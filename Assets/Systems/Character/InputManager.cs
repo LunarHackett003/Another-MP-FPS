9595cd53afab63cbe65e915ebb628f00bf7f42c9
LunarHackett003 <61294652+LunarHackett003@users.noreply.github.com>
2025-03-01 00:21:20 +0000

    Initial Commit 2: file boogaloo

---
+++ Assets/Systems/Character/InputManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : SingletonBehaviour<InputManager>
+{
+    private Vector2 lookInput;
+    private bool crouchInput;
+
+    public PlayerInput playerInput;
+    private Vector2 moveInput;
+    private bool fireInput;
+    private bool jumpInput;
+    private bool sprintInput;
+
+    public static Vector2 MoveInput { get => Instance.moveInput; set => Instance.moveInput = value; }
+    public static Vector2 LookInput { get => Instance.lookInput; set => Instance.lookInput = value; }
+    public static bool FireInput { get => Instance.fireInput; set => Instance.fireInput = value; }
+    public static bool JumpInput { get => Instance.jumpInput; set => Instance.jumpInput = value; }
+    public static bool SprintInput { get => Instance.sprintInput; set => Instance.sprintInput = value; }
+    public static bool CrouchInput { get => Instance.crouchInput; set => Instance.crouchInput = value; }
+
+
+
+
+    #region Input
+    public void GetMoveInput(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+    public void GetLookInput(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+    public void GetFireInput(InputAction.CallbackContext context)
+    {
+        fireInput = context.ReadValueAsButton();
+    }
+    public void GetJumpInput(InputAction.CallbackContext context)
+    {
+        jumpInput = context.ReadValueAsButton();
+    }
+    public void GetSprintInput(InputAction.CallbackContext context)
+    {
+        sprintInput = context.ReadValueAsButton();
+    }
+    public void GetCrouchInput(InputAction.CallbackContext context)
+    {
+        crouchInput = context.ReadValueAsButton();
+    }
+
+
+    #endregion
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
